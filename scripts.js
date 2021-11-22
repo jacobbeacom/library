d630ade6add06fde396dbd19b6ce02fc951bc47d
jacobbeacom <jdbeacom97@comcast.net>
2021-11-22 15:51:23 -0600

    Added javascript pseudocode describing the major functions needed for all library functionality.

---
+++ scripts.js
@@ -0,0 +1,43 @@
+let myLibrary = [];
+
+/*Book constructor, contains the title, author, pagecount, and
+whether or not the book has been read */
+
+function Book() {
+
+}
+
+/* Function to add a new book to the myLibrary array, should be
+a form */
+
+function addBookToLibrary() {
+
+}
+
+/* Function to display all book objects */
+
+function bookstoDisplay() {
+
+}
+
+/* Button to add new book to the library */
+    
+    // new book button
+
+/* Button to remove a book from the library (should appear on each)
+display */
+    
+    // remove book button
+
+
+/* Function to toggle the read status of a book */
+
+function toggleReadStatus() {
+    
+}
+
+/* button or toggle to toggle the read status of a book */
+
+    // toggle read button
+
+
