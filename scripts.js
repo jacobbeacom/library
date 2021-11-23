49c69115a4cc27ef41cb4663f43b5d6be99f402c
jacobbeacom <jdbeacom97@comcast.net>
2021-11-22 18:07:22 -0600

    Added the beginnings of the new book form to be access when user clicks the newbook button. Added basic constructor logic for book objects added a function to create a new book object and push it into the library array added beginnings of the add book button logic

--- scripts.js
+++ scripts.js
@@ -3,15 +3,18 @@ let myLibrary = [];
 /*Book constructor, contains the title, author, pagecount, and
 whether or not the book has been read */
 
-function Book() {
-
+function Book(title, author, pagecount, beenRead) {
+    this.title = title;
+    this.author = author;
+    this.pagecount = pagecount;
+    this.beenRead = beenRead;
 }
-
 /* Function to add a new book to the myLibrary array, should be
 a form */
 
 function addBookToLibrary() {
-
+    let newBook = object.create(Book);
+    myLibrary.push(newBook);
 }
 
 /* Function to display all book objects (make the divs)*/
@@ -22,7 +25,11 @@ function bookstoDisplay() {
 
 /* Button to add new book to the library */
     
-    // new book button
+const newBook = document.getElementById('addBook')
+
+newBook.addEventListener('click', () => {
+
+});
 
 /* Button to remove a book from the library (should appear on each)
 display */
@@ -33,7 +40,7 @@ display */
 /* Function to toggle the read status of a book */
 
 function toggleReadStatus() {
-
+    
 }
 
 /* button or toggle to toggle the read status of a book */
