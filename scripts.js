1a8cbe071f2dd988c8765b8d41b9939b3bd442a5
jacobbeacom <jdbeacom97@comcast.net>
2021-11-23 16:56:47 -0600

    Refactored some of the JS code for readability. Added and completed functionality for the Add Book button, button now appropriately adds a new object to the myLibrary array. Changed CSS properties for the Add Book pop-up prompt to always have the form hidden upon loading the page, and when clicking the button, the form will always display in the center of the user's screen.

--- scripts.js
+++ scripts.js
@@ -12,24 +12,53 @@ function Book(title, author, pagecount, beenRead) {
 /* Function to add a new book to the myLibrary array, should be
 a form */
 
-function addBookToLibrary() {
-    let newBook = object.create(Book);
-    myLibrary.push(newBook);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 
-/* Function to display all book objects (make the divs)*/
+/* Function to display all book objects */
 
 function bookstoDisplay() {
-
+    for (i=0; i < myLibrary.length; i++) {
+        
+    }
 }
 
 /* Button to add new book to the library */
     
-const newBook = document.getElementById('addBook')
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
 
-newBook.addEventListener('click', () => {
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
 
+
+let addBook = document.getElementById("addBook")
+
+addBook.addEventListener('click', openForm) 
+    
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pagecount = document.getElementById("pagecount");
+let beenReadButton = document.getElementById("beenRead");
+let submitButton = document.getElementById("submit");
+let beenRead = false;
+    
+beenReadButton.addEventListener('click', toggleReadStatus);
+
+submitButton.addEventListener('click', () => {
+        let newBook = Object.create(Book);
+            newBook.title = title.value;
+            newBook.author = author.value;
+            newBook.pagecount = pagecount.value;
+            newBook.beenRead = beenRead;
+    addBookToLibrary(newBook);
+    closeForm();
 });
+
+    
 
 /* Button to remove a book from the library (should appear on each)
 display */
@@ -40,11 +69,17 @@ display */
 /* Function to toggle the read status of a book */
 
 function toggleReadStatus() {
+    if (beenRead != true) {
+        beenRead = true;
+    } else {
+        beenRead = false;
     
-}
+}};
 
 /* button or toggle to toggle the read status of a book */
 
     // toggle read button
 
+
+//Closes the popup form when adding a new book
 
